9c99f0abd34800552d332c39e3b1615361132a40
Shohjahon <example@gmail.com>
2023-10-27 21:32:03 +0500

    .

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 app.listen(3000, function () {
-  console.log("Example app listening on port " + port + "!");
+  console.log("Example app listening on port 3000");
 });
 
 const token = process.env.TOKEN;
